7dbe8846656b6507a4f31fd10e614b82ab3af355
bierxiensi <bierxiensi@163.com>
2021-08-17 15:47:44 +0800

    fix
    Signed-off-by: bierxiensi <bierxiensi@163.com>

--- src/components/BlockList.vue
+++ src/components/BlockList.vue
@@ -19,76 +19,38 @@
 						</div> -->
           </div>
           <div class="block_list_pagination_content">
-            <el-table
-              class="table"
-              :data="blockList"
-              :empty-text="$t('ExplorerLang.table.emptyDescription')"
-            >
-              <el-table-column
-                :min-width="ColumnMinWidth.blockListHeight"
-                :label="$t('ExplorerLang.table.block')"
-              >
+            <el-table class="table" :data="blockList" :empty-text="$t('ExplorerLang.table.emptyDescription')">
+              <el-table-column :min-width="ColumnMinWidth.blockListHeight" :label="$t('ExplorerLang.table.block')">
                 <template slot-scope="scope">
                   <router-link :to="`/block/${scope.row.height}`">{{
                     scope.row.height
                   }}</router-link>
                 </template>
               </el-table-column>
-              <el-table-column
-                class-name="address"
-                v-if="productionConfig.blockList.proposer"
-                :min-width="ColumnMinWidth.proposer"
-                :label="$t('ExplorerLang.table.proposer')"
-              >
+              <el-table-column class-name="address" v-if="productionConfig.blockList.proposer" :min-width="ColumnMinWidth.proposer" :label="$t('ExplorerLang.table.proposer')">
                 <template slot-scope="scope">
-                  <span
-                    v-if="
+                  <span v-if="
                       scope.row.proposerAddress !== '' &&
                       scope.row.proposerAddress !== '--'
-                    "
-                  >
-                    <router-link
-                      class="common_link_style"
-                      :to="`/staking/${scope.row.proposerAddress}`"
-                      >{{ scope.row.proposerValue }}</router-link
-                    >
+                    ">
+                    <router-link class="common_link_style" :to="`/staking/${scope.row.proposerAddress}`">{{ scope.row.proposerValue }}</router-link>
                   </span>
-                  <span
-                    v-if="
+                  <span v-if="
                       scope.row.proposerAddress === '' &&
                       scope.row.proposerValue
-                    "
-                    >{{ scope.row.proposerValue }}</span
-                  >
+                    ">{{ scope.row.proposerValue }}</span>
                   <span v-if="scope.row.proposerAddress === '--'">--</span>
                 </template>
               </el-table-column>
-              <el-table-column
-                :min-width="ColumnMinWidth.txn"
-                prop="numTxs"
-                :label="$t('ExplorerLang.table.transactions')"
-              ></el-table-column>
+              <el-table-column :min-width="ColumnMinWidth.txn" prop="numTxs" :label="$t('ExplorerLang.table.transactions')"></el-table-column>
               <!-- <el-table-column v-if="productionConfig.blockList.validtors" :min-width="ColumnMinWidth.validatorValue" prop="validatorValue" :label="$t('ExplorerLang.table.validators')"></el-table-column> -->
               <!-- <el-table-column v-if="productionConfig.blockList.votingPower" :min-width="ColumnMinWidth.votingPowerValue" prop="votingPowerValue" :label="$t('ExplorerLang.table.votingPower')"></el-table-column> -->
-              <el-table-column
-                :min-width="ColumnMinWidth.time"
-                prop="time"
-                :label="$t('ExplorerLang.table.createTime')"
-              ></el-table-column>
-              <el-table-column
-                :min-width="ColumnMinWidth.blockAge"
-                prop="ageTime"
-                :label="$t('ExplorerLang.table.age')"
-              ></el-table-column>
+              <el-table-column :min-width="ColumnMinWidth.time" prop="time" :label="$t('ExplorerLang.table.createTime')"></el-table-column>
+              <el-table-column :min-width="ColumnMinWidth.blockAge" prop="ageTime" :label="$t('ExplorerLang.table.age')"></el-table-column>
             </el-table>
           </div>
           <div class="pagination_content">
-            <m-pagination
-              :page-size="pageSize"
-              :total="dataCount"
-              :page="pageNumber"
-              :page-change="pageChange"
-            ></m-pagination>
+            <m-pagination :page-size="pageSize" :total="dataCount" :page="pageNumber" :page-change="pageChange"></m-pagination>
           </div>
         </div>
       </div>
@@ -97,15 +59,15 @@
 </template>
 
 <script>
-import Tools from "../util/Tools";
-import MPagination from "./common/MPagination";
-import { getRangeBlockList, getLatestBlock } from "../service/api";
-import { ColumnMinWidth } from "../constant";
-import productionConfig from "@/productionConfig.js";
-import { validatePositiveInteger } from "../helper/IritaHelper";
+import Tools from '../util/Tools'
+import MPagination from './common/MPagination'
+import { getRangeBlockList, getLatestBlock } from '../service/api'
+import { ColumnMinWidth } from '../constant'
+import productionConfig from '@/productionConfig.js'
+import { validatePositiveInteger } from '../helper/IritaHelper'
 
 export default {
-  name: "BlockList",
+  name: 'BlockList',
   components: { MPagination },
   data() {
     return {
@@ -117,35 +79,35 @@ export default {
       latestBlockHeight: 0,
       dbHeight: 0,
       blockList: [],
-      blockListTimer: null
-    };
+      blockListTimer: null,
+    }
   },
   mounted() {
-    this.queryBlockList();
+    this.queryBlockList()
   },
   methods: {
     async queryBlockList() {
-      this.getBlocksCount();
-      await this.latestBlock();
-      this.getBlocks();
+      this.getBlocksCount()
+      await this.latestBlock()
+      this.getBlocks()
     },
     async getBlocks() {
-      let start = this.dbHeight - (this.pageNumber - 1) * this.pageSize;
-      let end = start - this.pageSize;
+      let start = this.dbHeight - (this.pageNumber - 1) * this.pageSize
+      let end = start - this.pageSize
       try {
         let blockList = await getRangeBlockList(
           start,
           validatePositiveInteger(end)
-        );
+        )
         if (blockList?.data && blockList?.data.length > 0) {
           if (blockList.data.length > this.pageSize) {
-            blockList.data = blockList.data.slice(0, this.pageSize);
+            blockList.data = blockList.data.slice(0, this.pageSize)
           }
-          this.blockList = blockList.data.map(item => {
+          this.blockList = blockList.data.map((item) => {
             return {
-              proposerAddress: item.proposer_addr || "--",
+              proposerAddress: item.proposer_addr || '--',
               proposerValue:
-                item.proposer_moniker || item.proposer_addr || "--",
+                item.proposer_moniker || item.proposer_addr || '--',
               // validatorValue: `${item.precommit_validator_num || 0} / ${item.total_validator_num || 0}`,
               // votingPowerValue: item.precommit_voting_power ? `${Tools.formatPerNumber((Number(item.precommit_voting_power) / item.total_voting_power) * 100)} %` : '--',
               height: item.height,
@@ -155,58 +117,58 @@ export default {
               ageTime: Tools.formatAge(
                 Tools.getTimestamp(),
                 item.time * 1000,
-                this.$t("ExplorerLang.table.suffix"),
-                ""
-              )
-            };
-          });
+                this.$t('ExplorerLang.table.suffix'),
+                ''
+              ),
+            }
+          })
         }
-        clearInterval(this.blockListTimer);
+        clearInterval(this.blockListTimer)
         this.blockListTimer = setInterval(() => {
-          this.blockList.map(item => {
+          this.blockList.map((item) => {
             item.ageTime = Tools.formatAge(
               Tools.getTimestamp(),
               item.Time * 1000,
-              this.$t("ExplorerLang.table.suffix"),
-              ""
-            );
-            return item;
-          });
-        }, 1000);
+              this.$t('ExplorerLang.table.suffix'),
+              ''
+            )
+            return item
+          })
+        }, 1000)
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
     async getBlocksCount() {
       try {
-        let data = await getRangeBlockList(null, null, true);
+        let data = await getRangeBlockList(null, null, true)
         if (data?.count) {
-          this.dataCount = data.count;
+          this.dataCount = data.count
         }
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
     async latestBlock() {
       try {
-        let blockData = await getLatestBlock();
+        let blockData = await getLatestBlock()
         if (blockData) {
-          this.latestBlockHeight = blockData.height;
-          this.dbHeight = blockData.dbHeight;
+          this.latestBlockHeight = blockData.height
+          this.dbHeight = blockData.dbHeight
         }
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     },
     pageChange(pageNum) {
       if (this.pageNumber == pageNum) {
-        return;
+        return
       }
-      this.pageNumber = pageNum;
-      this.getBlocks();
-    }
-  }
-};
+      this.pageNumber = pageNum
+      this.getBlocks()
+    },
+  },
+}
 </script>
 
 <style scoped lang="scss">
