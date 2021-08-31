aa3412a33f9ff81046cdf99de9d640b6ada4879c
Bierxiensi <40264423+Bierxiensi@users.noreply.github.com>
2021-08-31 14:05:54 +0800

    fix

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -11,7 +11,7 @@ const store = new Vuex.Store({
             symbol:'',
             isMobile:false,
         },
-        mainToken: '',
+        mainToken:'',
     },
     mutations : {
         SET_TEMP_DENOM_ID(state, data){
