b43d8a0e9c8fe3efadff4e1352f29908326227cf
jinbiao <zjb742626083>
2021-09-26 10:47:35 +0800

    change table list

---
+++ src/components/tableListColumnConfig/nativeAsseTxColumnConfig.js
@@ -0,0 +1,43 @@
+export default [
+    {
+        label:'TxHash',
+        displayValue:'',
+        nativeValue:''
+    },
+    {
+        label:'Owner',
+        displayValue: '',
+        nativeValue: '',
+    },
+    {
+        label: 'Symbol',
+        displayValue: '',
+        nativeValue: ''
+    },
+    {
+        label: 'InitialSupply',
+        displayValue: '',
+        nativeValue: ''
+    },
+    {
+        label: 'MinTable',
+        displayValue: '',
+        nativeValue: ''
+    },
+    {
+        label: 'Block',
+        displayValue: '',
+        nativeValue: ''
+    },
+    {
+        label: 'Fee',
+        displayValue: '',
+        nativeValue: ''
+    },
+    {
+        label: 'Age',
+        displayValue: '',
+        nativeValue: ''
+    },
+
+]
